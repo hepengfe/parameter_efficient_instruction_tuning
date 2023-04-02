09b7f479649eb6918b63fe506fb1d5315d4712cc
hepengfe <hepengfe@uw.edu>
2023-04-02 02:42:54 -0700

    AdapterPEFTModel.py

---
+++ AdapterPEFTModel.py
@@ -0,0 +1,14 @@
+from peft.peft_model import (
+    PeftModel,
+    PeftModelForCausalLM,
+    PeftModelForSeq2SeqLM,
+    PeftModelForSequenceClassification,
+    PeftModelForTokenClassification,
+)
+from transformers import AutoModelForSeq2SeqLM, AutoAdapterModel
+
+class AdapterPeftModelForSeq2Seq(AutoAdapterModel, PeftModel):
+    def __init__(self, config, adapter_config=None):
+        AutoAdapterModel.__init__(self, config, adapter_config)
+        PeftModel.__init__(self, config)
+    
