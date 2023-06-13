cc2954565674d547e16b3e2dc910557b5755d7b8
murphy <hepengfe@gmail.com>
2023-06-13 10:09:11 -0700

    opt adapter implementation

--- src/peft/tuners/__init__.py
+++ src/peft/tuners/__init__.py
@@ -21,3 +21,4 @@ from .lora import LoraConfig, LoraModel
 from .p_tuning import PromptEncoder, PromptEncoderConfig, PromptEncoderReparameterizationType
 from .prefix_tuning import PrefixEncoder, PrefixTuningConfig
 from .prompt_tuning import PromptEmbedding, PromptTuningConfig, PromptTuningInit
+from .adapter import AdapterConfig, AdapterModel
