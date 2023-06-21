f2e5bb6a85b36909a5d3fd1643bcb5facf6c611a
hepengfe <hepengfe@uw.edu>
2023-06-21 15:36:16 -0700

    improve grid search 3 scripts

--- scripts/grid_search_3/grid_search_adapter_3.sh
+++ scripts/grid_search_3/grid_search_adapter_3.sh
@@ -14,6 +14,7 @@ export LABEL_SMOOTHING_FACTOR=0
 export DROPOUT_RATE=0.1
 export WEIGHT_DECAY=0.01
 export LR=1e-4
+export RANDOM_SEED=42
 adapter_size=256
 
 for model_name in "${MODEL_NAMES[@]}"; do
@@ -21,7 +22,7 @@ for model_name in "${MODEL_NAMES[@]}"; do
     export ADAPATER_SIZE=$adapter_size
     export CMD_INDEX=$i
     export MODEL_NAME=$model_name
-    bash scripts/hfai/hp_run.sh adapter $script_mode &
+    bash scripts/hfai/hp_run.sh adapter_peft $script_mode &
     ((i++))
     if [ $script_mode == "dev" ];then
         break
